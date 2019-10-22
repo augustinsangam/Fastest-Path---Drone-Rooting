d1c70de08183540f6e4146ee2d35e4f59d74c1b3
Eya-Tom Augustin SANGAM <augustin.sangam@gmail.com>
2019-10-22 10:37:27 -0400

    Test de commit

--- LOG2810_TP1/main.py
+++ LOG2810_TP1/main.py
@@ -3,8 +3,12 @@ from Chemin import Chemin
 
 
 if __name__ == "__main__" :
-    entrepot = Entrepot()
-    parcours = Chemin(entrepot)
 
-    for element in parcours.parcoursOrdonne(0) :
-        print(element)
+    #entrepot = Entrepot()
+    #parcours = Chemin(entrepot)
+
+    #for element in parcours.parcoursOrdonne(0) :
+    #    print(element)
+    
+    pass
+    
